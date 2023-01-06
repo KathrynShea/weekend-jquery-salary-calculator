9781d26bfe7118a90a683337aeb9e11e639c3223
Kathryn Szombatfalvy <kathrynszombatfalvy@gmail.com>
2023-01-06 16:48:57 -0600

    added delete button and tracked monthly cost total

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 let allEmployees = [];
+let totalAnnualCost = 0;
+let totalMonthlyCost = 0;
 
 $(function(){
  console.log("JS and JQ working")
@@ -13,7 +15,7 @@ function addEmployee(){
     let employeeLastName = $("#employeeLastName").val();
     let employeeID = $("#employeeID").val();
     let jobTitle = $("#jobTitle").val();
-    let annualSalary = $("#annualSalary").val();
+    let annualSalary = Number($("#annualSalary").val());
 
     //clearing DOM
     $("#employeeFirstName").val("");
@@ -22,19 +24,46 @@ function addEmployee(){
     $("#jobTitle").val("");
     $("#annualSalary").val("");
 
+    //add new employee to global array
     allEmployees.push({employeeFirstName, employeeLastName, employeeID, jobTitle, annualSalary})
 
     appendDOM();
+    updateMonthlyCost();
 }
 
-
 function appendDOM(){
+    //clear table on DOM
     $(".employeeRow").empty();
 
+    //loop through global array and add all employees to table on DOM
     for (employee of allEmployees){
         $(".employeeRow").append(`<tr><td>${employee.employeeFirstName}</td>
         <td>${employee.employeeLastName}</td><td>${employee.employeeID}</td><td>${employee.jobTitle}</td>
-        <td>${employee.annualSalary}</td></tr>`);
+        <td>${employee.annualSalary}</td><td><button id ="somthing special">Delete</button></td></tr>`);
+    }
+}
+
+function updateMonthlyCost(){
+    totalAnnualCost = 0
     
-    }}
+    //add all employee annual salaries
+    //let rowClass = "normal-budget";
+
+   /* if (totalMonthlyCost > 20,000){
+        .warning
+    }
+    */
+
+    for (employee of allEmployees){
+        totalAnnualCost += employee.annualSalary;
+        
+    }
+
+    //divide total of annual salaries by 12 to get monthly cost
+    // learned toFixed() from https://stackoverflow.com/questions/11832914/how-to-round-to-at-most-2-decimal-places-if-necessary
+    totalMonthlyCost = (totalAnnualCost / 12).toFixed(2);
+
+    //update the total cost on the DOM
+    $(".monthlyCostTotal").text(totalMonthlyCost);
+}
 
